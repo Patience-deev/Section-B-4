52b1d95f3e72f327a5be2a01548365bb846c4912
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-16 13:47:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_B_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(3));
+            Console.WriteLine(test(14));
+            Console.WriteLine(test(12));
+            Console.WriteLine(test(37));
+            Console.ReadLine();
+        }
+        public static bool test(int n)
+        {
+            return n % 3 == 0 || n % 7 == 0;
+        }
+    }
+}
